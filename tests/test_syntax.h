68adaad6860c375bde9f2bd94c495fcd0921223d
ylilt <ylilt71@gmail.com>
2016-11-18 16:46:18 +0100

    added libtool & googletest support

---
+++ tests/test_syntax.h
@@ -0,0 +1,48 @@
+/*
+ *  MIT License
+ *
+ * Copyright (c) 2016 .DRY (Don't Repeat Yourself)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* $Id$ */
+
+#ifndef _SYNTAX_H
+#define _SYNTAX_H
+
+#define SYNTAX_TEST(index, ex_status, ex_cerr, code) \
+  TEST(syntax, syntax##index) { \
+    ddc::driver driver = ddc::driver(); \
+    testing::internal::CaptureStderr(); \
+    ASSERT_EQ(ex_status, driver.parse_string(code)); \
+    std::string output = testing::internal::GetCapturedStderr(); \
+    ASSERT_EQ(ex_cerr, output); \
+  } \
+
+#define RUN  \
+  int main(int argc, char **argv) { \
+    ::testing::InitGoogleTest(&argc, argv); \
+    return RUN_ALL_TESTS(); \
+  }
+
+#endif /* _SYNTAX_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */

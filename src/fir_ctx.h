dec00dad3372035636e17c5c49112782338f8804
uael <lucas.abel@yandex.com>
2017-02-16 12:33:01 +0100

    fir_ctx_t header clean up

--- src/fir_ctx.h
+++ src/fir_ctx.h
@@ -1,23 +1,36 @@
-#ifndef _JAYL_CONTEXT_H_
-#define _JAYL_CONTEXT_H_
+#ifndef _JAYL_FIR_CTX_H_
+#define _JAYL_FIR_CTX_H_
 
-#include <stdbool.h>
+#include <p99.h>
 
 struct _fir_lexer_t;
 struct _fir_parser_t;
 
-typedef struct _ctx_t {
+/**
+ * The type struct fir_ctx_t.
+ * @brief Contains all program options and ref to lexer and parser.
+ */
+typedef struct _fir_ctx_t {
   const char *program;
   const char *in;
   const char *out;
   char src_dir[256];
   bool verbose;
+
   struct _fir_lexer_t *lexer;
   struct _fir_parser_t *parser;
 } fir_ctx_t;
 
+/**
+ * fir_ctx_t ctor.
+ * @param this The fir_ctx_t ptr to ctor.
+ */
 void ctx_ctor(fir_ctx_t *this);
 
+/**
+ * fir_ctx_t dtor.
+ * @param this The fir_ctx_t ptr to ctor.
+ */
 void ctx_dtor(fir_ctx_t *this);
 
-#endif /* _JAYL_CONTEXT_H_ */
+#endif /* _JAYL_FIR_CTX_H_ */

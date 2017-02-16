27ff6dcf03628731efa669f4d9954842fbe07803
uael <lucas.abel@yandex.com>
2017-02-16 14:59:36 +0100

    ctor assertions

--- src/fir_lexer.c
+++ src/fir_lexer.c
@@ -7,6 +7,9 @@ void fir_lexer_ctor(fir_lexer_t *this, fir_ctx_t *ctx) {
 }
 
 void fir_lexer_ctor2(fir_lexer_t *this, fir_ctx_t *ctx, const char *in) {
+  assert(this);
+  assert(ctx);
+
   this->ctx = ctx;
   this->filename = in;
   this->lex = jay_lex;
@@ -14,5 +17,5 @@ void fir_lexer_ctor2(fir_lexer_t *this, fir_ctx_t *ctx, const char *in) {
 }
 
 void fir_lexer_dtor(fir_lexer_t *this) {
-
+  assert(this);
 }
--- src/fir_parser.c
+++ src/fir_parser.c
@@ -1,56 +1,56 @@
 #include "fir_parser.h"
 
-#include <stdlib.h>
 #include <assert.h>
 
 #include "frontend/jay_parser.h"
 
-#define _peek() deque_front(&this->toks)
-#define _peekn(n) deque_get(&this->toks, n)
-#define _next() deque_pop_front(&this->toks)
-#define _consume(tok) ({ \
-    if (_peek().kind != tok) { \
+#define __peek() deque_front(&this->toks)
+#define __next() deque_pop_front(&this->toks)
+
+static fir_tok_t peek(struct _fir_parser_t *this) {
+  return deque_front(&this->toks);
+}
+
+static fir_tok_t peekn(struct _fir_parser_t *this, unsigned n) {
+  return deque_get(&this->toks, n);
+}
+
+static fir_tok_t next(struct _fir_parser_t *this) {
+  return deque_pop_front(&this->toks);
+}
+
+static fir_tok_t consume(struct _fir_parser_t *this, fir_tok tok) {
+  if (__peek().kind != tok) { \
       puts("unexpected token"); \
       exit(1); \
     } \
-    _next(); _peek(); \
-  })
-
-static inline fir_tok_t __peek(struct _fir_parser_t *this) {
-  return _peek();
-}
-
-static inline fir_tok_t __peekn(struct _fir_parser_t *this, unsigned n) {
-  return _peekn(n);
-}
-
-static inline fir_tok_t __next(struct _fir_parser_t *this) {
-  return _next();
-}
-
-static inline fir_tok_t __consume(struct _fir_parser_t *this, fir_tok tok) {
-  return _consume(tok);
+    __next();
+  return __peek();
 }
 
 void fir_parser_ctor(fir_parser_t *this, fir_ctx_t *ctx) {
-  assert(ctx->lexer);
   fir_parser_ctor2(this, ctx, ctx->lexer, ctx->src_dir);
 }
 
 void fir_parser_ctor2(fir_parser_t *this, fir_ctx_t *ctx, fir_lexer_t *lexer, char *src_dir) {
+  assert(this);
+  assert(ctx);
+  assert(lexer);
+  assert(src_dir);
+
   this->ctx = ctx;
   this->lexer = lexer;
 
   strcpy(this->src_dir, src_dir);
 
-  this->peek = __peek;
-  this->peekn = __peekn;
-  this->next = __next;
-  this->consume = __consume;
+  this->peek = peek;
+  this->peekn = peekn;
+  this->next = next;
+  this->consume = consume;
   this->parse = jay_parse;
   this->parse_str = jay_parse_str;
 }
 
 void fir_parser_dtor(fir_parser_t *parser) {
-
+  assert(parser);
 }

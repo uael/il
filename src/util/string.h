4d44eaa828b7df78cc65a724564de1ca519634a1
uael <lucas.abel@yandex.com>
2017-03-11 20:42:24 +0100

    license

--- src/util/string.h
+++ src/util/string.h
@@ -1,16 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2017 uael <www.github.com/uael>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef   JL_STRING_H__
+# define  JL_STRING_H__
+
 #include <string.h>
 #include <malloc.h>
 
 #if !defined(__GNUC__)
-char * strndup (const char *s, size_t n) {
-  char *result;
-  size_t len = strnlen (s, n);
+  char * strndup (const char *s, size_t n) {
+    char *result;
+    size_t len = strnlen (s, n);
 
-  result = (char *) malloc (len + 1);
-  if (!result)
-    return 0;
+    result = (char *) malloc (len + 1);
+    if (!result)
+      return 0;
 
-  result[len] = '\0';
-  return (char *) memcpy (result, s, len);
-}
+    result[len] = '\0';
+    return (char *) memcpy (result, s, len);
+  }
 #endif
+
+#endif /* JL_STRING_H__ */

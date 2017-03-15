f0252927dc166286392bf1abca5f9cfdc6b9ed27
uael <lucas.abel@yandex.com>
2017-03-15 22:10:01 +0100

    pp in one proc

--- src/frontend/c/c_pp.c
+++ src/frontend/c/c_pp.c
@@ -34,11 +34,9 @@ void c_pp_dtor(c_pp_t *self) {
   jl_lexer_dtor(&self->pp_lexer);
 }
 
-void c_pp_parse_define(c_pp_t *self, c_macro_t *macro);
-
 
 bool c_pp_op_push_callback(jl_lexer_event_t *self, void *arg) {
-  jl_token_t *token, tok;
+  jl_token_t *token, t, pt;
   c_pp_t *pp;
   c_macro_t macro = {C_MACRO_UNDEFINED};
   unsigned it;
@@ -50,26 +48,52 @@ bool c_pp_op_push_callback(jl_lexer_event_t *self, void *arg) {
   if (token->type == '#') {
     jl_lexer_fork(&pp->pp_lexer, self->lexer);
     jl_lexer_skip(&pp->pp_lexer, token->length);
-    if ((tok = jl_lexer_peek(&pp->pp_lexer)).kind == Jl_TOKEN_IDENTIFIER) {
-      if (strcmp("define", tok.s) == 0) {
-        c_pp_parse_define(pp, &macro);
+    if ((t = jl_lexer_peek(&pp->pp_lexer)).kind == Jl_TOKEN_IDENTIFIER) {
+      if (strcmp("define", t.s) == 0) {
+        t = jl_lexer_consume(&pp->pp_lexer, C_TOK_IDENTIFIER);
+        macro.name = t.s;
+
+        if (jl_lexer_peek(&pp->pp_lexer).type == '(') {
+          macro.kind = C_MACRO_FUNC;
+          lbl_parse_arg:
+          jl_vector_push(macro.params, jl_lexer_consume(&pp->pp_lexer, C_TOK_IDENTIFIER));
+          if (jl_lexer_peek(&pp->pp_lexer).type == ',') {
+            jl_lexer_consume(&pp->pp_lexer, ',');
+            goto lbl_parse_arg;
+          }
+          jl_lexer_consume(&pp->pp_lexer, ')');
+        } else {
+          macro.kind = C_MACRO_OBJECT;
+        }
+
+        while ((t = jl_lexer_next(&pp->pp_lexer)).type != 0) {
+          if (t.type == '\n') {
+            if (pt.type != '\\') break;
+            else {
+              jl_token_dtor(&t);
+              continue;
+            }
+          }
+          jl_vector_push(macro.replacement, t);
+          pt = t;
+        }
         it = kh_put(c_macro_ht, &pp->macros, macro.name, &out);
         if (out == 0) {
           puts("macro redefined");
         }
         kh_value(&pp->macros, it) = macro;
         jl_lexer_join(self->lexer, &pp->pp_lexer);
-        jl_token_dtor(&tok);
+        jl_token_dtor(&t);
 
         return false;
-      } else if (strcmp("undef", tok.s) == 0) {
-        tok = jl_lexer_consume(&pp->pp_lexer, C_TOK_IDENTIFIER);
-        it = kh_get(c_macro_ht, &pp->macros, tok.s);
+      } else if (strcmp("undef", t.s) == 0) {
+        t = jl_lexer_consume(&pp->pp_lexer, C_TOK_IDENTIFIER);
+        it = kh_get(c_macro_ht, &pp->macros, t.s);
         if (it != kh_end(&pp->macros)) {
           kh_del(c_macro_ht, &pp->macros, it);
         }
         jl_lexer_join(self->lexer, &pp->pp_lexer);
-        jl_token_dtor(&tok);
+        jl_token_dtor(&t);
 
         return false;
       }
@@ -78,8 +102,8 @@ bool c_pp_op_push_callback(jl_lexer_event_t *self, void *arg) {
     it = kh_get(c_macro_ht, &pp->macros, token->s);
     if (it != kh_end(&pp->macros)) {
       macro = kh_value(&pp->macros, it);
-      jl_vector_foreach(macro.replacement, tok) {
-        jl_deque_push(self->lexer->queue, tok);
+      jl_vector_foreach(macro.replacement, t) {
+        jl_deque_push(self->lexer->queue, t);
       }
       return false;
     }
@@ -93,38 +117,6 @@ void c_pp_op_push_dtor(jl_lexer_event_t *self) {
     c_pp_dtor(self->data);
     free(self->data);
     self->data = NULL;
-  }
-}
-
-void c_pp_parse_define(c_pp_t *self, c_macro_t *macro) {
-  jl_token_t token, prev_token;
-
-  token = jl_lexer_consume(&self->pp_lexer, C_TOK_IDENTIFIER);
-  macro->name = token.s;
-
-  if (jl_lexer_peek(&self->pp_lexer).type == '(') {
-    macro->kind = C_MACRO_FUNC;
-    lbl_parse_arg:
-    jl_vector_push(macro->params, jl_lexer_consume(&self->pp_lexer, C_TOK_IDENTIFIER));
-    if (jl_lexer_peek(&self->pp_lexer).type == ',') {
-      jl_lexer_consume(&self->pp_lexer, ',');
-      goto lbl_parse_arg;
-    }
-    jl_lexer_consume(&self->pp_lexer, ')');
-  } else {
-    macro->kind = C_MACRO_OBJECT;
-  }
-
-  while ((token = jl_lexer_next(&self->pp_lexer)).type != 0) {
-    if (token.type == '\n') {
-      if (prev_token.type != '\\') break;
-      else {
-        jl_token_dtor(&token);
-        continue;
-      }
-    }
-    jl_vector_push(macro->replacement, token);
-    prev_token = token;
   }
 }
 

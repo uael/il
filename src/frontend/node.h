f1e2ad92ea66537b2bba32e5532b391b7c81afa4
uael <lucas.abel@yandex.com>
2017-04-03 17:45:14 +0200

    anonymous struct portability test

---
+++ src/frontend/node.h
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2017 uael <www.github.com/uael>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef   JL_NODE_H__
+# define  JL_NODE_H__
+
+#include "entity_t.h"
+#include "expr_t.h"
+#include "stmt_t.h"
+#include "type_t.h"
+
+typedef struct jl_node jl_node_t;
+
+enum jl_node_n {
+  JL_NODE_UNDEFINED = 0,
+  JL_NODE_ENTITY,
+  JL_NODE_EXPR,
+  JL_NODE_TYPE
+};
+
+struct jl_node {
+  enum jl_node_n kind;
+  union {
+    struct {
+      unsigned int vkind : 4;
+      jl_lloc_t lloc;
+    };
+    jl_entity_t entity;
+    jl_expr_t expr;
+    jl_stmt_t stmt;
+    jl_type_t type;
+  };
+};
+
+#endif /* JL_NODE_H__ */

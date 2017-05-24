3f445d1fbb6315f0fe8c32d174c4f4f5d945fe2c
uael <lucas.abel@yandex.com>
2017-05-24 23:43:00 +0200

    updated license file header copyright

--- src/adt/string.h
+++ src/adt/string.h
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2016-2017 Lucas Abel <www.github.com/uael>
+ * Wulk - Wu uniform language kit
+ * Copyright (C) 2016-2017 Lucas Abel <www.github.com/uael>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, version 2.1.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Lesser Public License for more details.
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this library; if not, see <http://www.gnu.org/licenses/>
  */
 
 #ifndef   WULK_STRING_H__

6ae3c939c28fbb471efc2399c3e8a3ff47593906
uael <lucas.abel@yandex.com>
2017-06-16 02:53:58 +0200

    cmake cleanup

---
+++ dependencies.cmake
@@ -0,0 +1,6 @@
+project_dependency(u LINK
+  GIT_REPOSITORY https://github.com/uael/mu.git
+  GIT_TAG master)
+project_dependency(cute TEST LINK
+  GIT_REPOSITORY https://github.com/uael/cute.git
+  GIT_TAG master)
